f0b6f19294a70299c82f9f82c05cd13e63926169
MyTruQs <raulfernandes.rm@gmail.com>
2024-10-22 22:34:34 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
 function App() {
   return (
-    <h1>Hello Wold</h1>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis modi soluta a odio delectus obcaecati. Molestias quae nisi cum eveniet rerum minus suscipit! Ipsa a deleniti assumenda dolores nostrum dicta?
+        </main>
+      </div>
+    </div>
   )
 }
 
